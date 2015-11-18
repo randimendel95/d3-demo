e3dcade7fa7d62cd43b86c72bd74a47b587372da
Randi <randi.mendel@yahoo.com>
2015-11-18 09:45:34 -0800

    inclass

--- js/main.js
+++ js/main.js
@@ -29,9 +29,14 @@ $(function() {
 	// Write a function to set your scales
 	var setScales = function() {
 		// xScale
-		
+		var xMin = d3.min(data,function(d) {return d.le_1960});
+		var xMax = d3.max(data,function(d) {return d.le_1960});
+		xScale = d3.scale.linear().domain([xMin,xMax]).range([0,width]);
 
-		// yScale		
+		// yScale	
+		var yMin = d3.min(data,function(d) {return d.le_2013});
+		var yMax = d3.max(data,function(d) {return d.le_2013});
+		yScale = d3.scale.linear().domain([yMin,yMax]).range([height,0])	
 	}
 
 	/* Write a function to define the positioning of your circles
@@ -40,34 +45,37 @@ $(function() {
 		- title attribute as the country of the object
 	*/
 	var circleFunc = function(circle) {
-		
-
-
+		//purely defining attributes
+		circle.attr('cx', function(d) {return xScale(d.le_1960)})
+		circle.attr('cy', function(d) {return yScale(d.le_2013)})
+		circle.attr('title', function(d) {return d.country})
+		circle.attr('r',7)
+		circle.attr('fill', 'black')
+		circle.attr('opacity',.3)
 	}
 
 	// Write a reusable drawing function for circles
 	var draw = function(data) {
 		// Set Scales
-		
+		setScales();
 		
 		// Select all circles and bind your data to them
-		
+		var circles = g.selectAll('circle').data(data)
 	
 		// Use the .enter() method to get your entering elements, and then position them using your positioning function
-    	
+    	circles.enter().append('circle').call(circleFunc)
 	
   
 	    // Use the .exit() and .remove() methods to remove elements that are no longer in the data
-		
+		circles.exit().remove()
 	  
 	    // Select all circle elements within your g and transition their position using your positioning function
-		
+		g.selectAll('circle').transition().duration(2000).call(circleFunc)
 
-	
 	}
 
 	// Pass data to your drawing function
-	
+	draw(data);
 
 	// Define x axis using d3.svg.axis(), assigning the scale as the xScale
 	
